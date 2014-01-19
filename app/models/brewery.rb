e40ef2db7da41d0930b9f09cd2948fc49672b025
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2014-01-19 21:59:19 +0200

    vk2 t10-14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,11 @@
 class Brewery < ActiveRecord::Base
-    has_many :beers
+    has_many :beers, :dependent => :destroy
+    has_many :ratings, :through => :beers
+
+    def average_rating
+        #ratings.average('score')
+        ratings.inject(0) { |sum, s| sum + s.score } / ratings.count
+    end
 
     def to_s
         "#{name}"
