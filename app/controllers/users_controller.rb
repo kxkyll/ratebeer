c705f839617f5d7be63134eda0b904b50436fbc6
Kati Kyllönen <kati.a.kyllonen@helsinki.fi>
2014-02-02 11:17:21 +0200

    wk3 t11-13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,8 +47,9 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    
     respond_to do |format|
-      if @user.update(user_params)
+      if @user == current_user && @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
@@ -61,7 +62,13 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @user = User.find(params[:id])
+
+    if @user == current_user
+      @user.destroy
+      session[:user_id] = nil
+    end
+
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
