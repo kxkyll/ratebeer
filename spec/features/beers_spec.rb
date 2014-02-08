47d16b01c795b39d3777954fbafcbc3a040655e1
Kati Kyllönen <kati.a.kyllonen@helsinki.fi>
2014-02-08 21:08:36 +0200

    vk4 t5 beer testit

---
+++ spec/features/beers_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "Beer" do
+    before :each do
+    FactoryGirl.create :brewery, name:"Koff"
+  end
+
+    it "when name given, is added to the system" do
+        visit new_beer_path
+        #save_and_open_page
+        fill_in('beer_name', with:'BeerToBeAdded')
+        
+
+        expect{
+          click_button('Create Beer')
+        }.to change{Beer.count}.by(1)
+    end
+
+    it "is not saved if name blank" do
+        visit new_beer_path
+       expect{
+          click_button('Create Beer')
+        }.to change{Beer.count}.by(0) 
+
+    end
+    it "is redirected back to new beer form if no name is given" do
+        visit new_beer_path
+        
+        expect{
+          click_button('Create Beer')
+        }.to change{Beer.count}.by(0) 
+        # tää polku vaikutaa oudolta
+        expect(current_path).to eq(beers_path)
+        expect(page).to have_content "New beer"
+        expect(page).to have_content "Name can't be blank"
+    end
+end
